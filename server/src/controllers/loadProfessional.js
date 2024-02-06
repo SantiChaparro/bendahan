cefe639329c9202cdb966f1232cbb431b6a94154
santiago <santiago@chaparro.com.ar>
2024-02-06 19:14:28 -0300

    mocks de carga y loaders funcionando

---
+++ server/src/controllers/loadProfessional.js
@@ -0,0 +1,25 @@
+const mockProfessional = require('../mocks/mockProfessionals.json');
+
+const { Professional } = require('../db');
+
+const professionalLoader = async () => {
+
+    const professional = mockProfessional.professionals.map((professional)=>{
+
+        return {
+
+            dni: professional.dni,
+            name: professional.name,
+            phone: professional.phone,
+            mail: professional.mail
+        }
+
+    })
+
+    const loadProfessional = await Professional.bulkCreate(professional);
+    return loadProfessional;
+
+
+};
+
+module.exports = professionalLoader;
