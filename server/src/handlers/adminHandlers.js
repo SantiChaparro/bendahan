983670410e7cbbd95e084ee913d7f357b5dd98d8
santiago <santiago@chaparro.com.ar>
2024-02-06 20:20:42 -0300

    funcionalidades get clients y postClient

---
+++ server/src/handlers/adminHandlers.js
@@ -0,0 +1,40 @@
+const {getAllClients,newClient} = require('../controllers/adminControllers');
+
+const getClients =async (req,res) => {
+
+    try {
+        const clients = await getAllClients();
+
+        if(clients){
+
+        res.status(200).json(clients);
+
+        }else{
+
+        throw new Error('Problema al cargar los clientes');
+        }
+    } catch (error) {
+        res.status(500).send({error:error.message})
+    }
+
+};
+
+const postClient = async (req,res) => {
+
+    const {dni,name,DateOfBirth,phone,mail} = req.body;
+
+    try {
+        
+        const client = await newClient(dni,name,DateOfBirth,phone,mail);
+        
+        if(client){
+            res.status(200).json(client);
+        }
+
+    } catch (error) {
+        res.status(500).send({error:error.message});
+    }
+
+};
+
+module.exports = {getClients,postClient};
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,7 @@
 const {Router} = require('express');
-
+const adminRouter = require('./adminRouter');
 const router = Router();
+
+router.use('/admin',adminRouter);
 
 module.exports = router;
