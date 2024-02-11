063737b836ce4d77609dcd341ceb881318565cb0
santiago <santiago@chaparro.com.ar>
2024-02-11 19:45:33 -0300

    postPayment listo asociando pago con turnos

--- server/src/controllers/paymentControllers.js
+++ server/src/controllers/paymentControllers.js
@@ -1,5 +1,28 @@
-const {Payment}= require ('../db')
+const {Payment}= require ('../db');
+const {Appointment} = require('../db');
 
-//controllers de payment
+const postNewPayment = async (payment_day,amount,payment_mode,appointmentsId) => {
 
-module.exports={}
+    const payment = await Payment.create({payment_day,amount,payment_mode});
+
+    if(payment){
+        await Promise.all(appointmentsId.map(async (appointmentId) => {
+            const appointment = await Appointment.findByPk(appointmentId);
+            if (appointment) {
+                await appointment.setPayment(payment);
+            } else {
+                throw new Error(`No se encontró la cita con el ID ${appointmentId}`);
+            }
+        }));
+
+        const succesMessage = 'Pago registrado con éxito';
+
+        return {succesMessage,payment};
+
+    }
+    
+   
+
+};
+
+module.exports={postNewPayment}
