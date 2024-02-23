6659c65ee69c3f5078f75bbbbaf4c5d4659baed5
santiago <santiago@chaparro.com.ar>
2024-02-23 20:21:24 -0300

    intento de hacer algo

--- client/src/views/Customers/Customers.jsx
+++ client/src/views/Customers/Customers.jsx
@@ -1,9 +1,32 @@
-const Customers = ()=>{
+import { Button,Dialog,DialogActions,DialogContent,DialogContentText,DialogTitle } from "@mui/material";
+import React, { useState } from "react";
+//import {useSatate} from 'react';
+
+
+const Customers = ({clientesOpen, setClientesOpen})=>{
+
+   
+    
+
+    const closeClientModal = () =>{
+        setClientesOpen(false)
+    }
+
     return (
-    <>
-        <h1>Estas en Customers</h1>
-    </>
+        <div>
+            <Dialog open={clientesOpen} fullWidth maxWidth="lg">
+                <DialogTitle>Clientes</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>blavscxascl</DialogContentText>
+
+                </DialogContent>
+                <DialogActions>
+                   <Button color="error" variant="contained" onClick={closeClientModal}>close</Button>
+                </DialogActions>
+            </Dialog>
+            
+        </div>
     )
 }
 
-export default Customers;
+export default Customers; 
